78af0b6382608d9a9112b1fe82f8781350713da8
Gift <aghaulor.gift@gmail.com>
2024-07-23 20:03:41 +0000

    9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,17 +1,12 @@
 export default function guardrail(mathFunction) {
   const queue = [];
-
   try {
-    // Execute the mathFunction and push the result to the queue
     const result = mathFunction();
     queue.push(result);
   } catch (error) {
-    // If an error occurs, push the error message to the queue
-    queue.push(error.message);
+    queue.push(`Error: ${error.message}`);
   } finally {
-    // Always push this message to the queue
     queue.push('Guardrail was processed');
   }
-
   return queue;
 }
