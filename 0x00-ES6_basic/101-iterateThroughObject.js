a7643be2d6dffae253f45b15dfb6cd21e7e75297
Gift <aghaulor.gift@gmail.com>
2024-07-23 18:52:53 +0000

    101-iterateThroughObject.js

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,10 +1,12 @@
 export default function iterateThroughObject(reportWithIterator) {
   const employees = [];
   
+
   // Collect all employees into the array
   for (const employee of reportWithIterator) {
     employees.push(employee);
   }
+
 
   // Join the array into a string with ' | ' separator
   return employees.join(' | ');
