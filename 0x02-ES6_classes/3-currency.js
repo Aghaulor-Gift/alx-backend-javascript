e6f795a66ed2294945474b1da6a6d9d25db410ab
Gift <aghaulor.gift@gmail.com>
2024-07-29 04:51:47 +0000

    Update class currency

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,28 +1,25 @@
 class Currency {
   constructor(code, name) {
-    this._code = this.code;
-    this._name = this.name
+    this._code = code;
+    this._name = name;
   }
 
-  // Getter and Setter for code
   get code() {
     return this._code;
   }
 
   set code(code) {
-    this._code = this.code;
+    this._code = code;
   }
 
-  // Getter and Setter for name
   get name() {
     return this._name;
   }
 
   set name(name) {
-    this._name = this.name;
+    this._name = name;
   }
 
-  // Method to display the full currency
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
