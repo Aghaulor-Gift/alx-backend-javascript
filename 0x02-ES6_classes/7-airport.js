55acd3fbcb40fef0e868ecb3b7fbd97f12857f18
Gift <aghaulor.gift@gmail.com>
2024-07-29 05:41:27 +0000

    Add to string method

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,20 +1,10 @@
-class Airport {
+export default class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
   }
 
-  get name() {
-    return this._name;
-  }
-
-  get code() {
-    return this._code;
-  }
-
   toString() {
-    return this._code;
+    return `[object ${this._code}]`;
   }
 }
-
-export default Airport;
