35495b71666e40309257e67d870675c1ff68b7e5
Gift <aghaulor.gift@gmail.com>
2024-07-29 05:25:08 +0000

    Update class building

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,12 @@
-class Building {
+export default class Building {
   constructor(sqft) {
-    if (this.constructor === Building) {
-      throw new Error('Building is an abstract class and cannot be instantiated directly');
-    }
     this._sqft = sqft;
+    if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
+    }
   }
 
   get sqft() {
     return this._sqft;
   }
-
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
-  }
-
-export default Building;
+}
