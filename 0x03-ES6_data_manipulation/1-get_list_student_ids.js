450ad05c87b2aa5d100f1aa6fb57378cd4137cdb
Gift <aghaulor.gift@gmail.com>
2024-08-02 18:27:19 +0000

    Add map to the array

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,8 @@
+const getListStudentIds = (studentArray) => {
+  if (!Array.isArray(studentArray)) {
+    return [];
+  }
+  return myArray.map((studentId) => studentId.id);
+};
+
+export default getListStudentIds;
