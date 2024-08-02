0a32772749a588f5d19cf41fff00f301454a8c39
Gift <aghaulor.gift@gmail.com>
2024-08-02 18:53:30 +0000

    Add filter to array object

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+const getStudentsByLocation (students, city) {
+  const arrayObject = students.filter((obj) => obj.location === city);
+    return arrayObject;
+};
+
+export default getStudentsByLocation;
