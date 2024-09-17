c5c53dde034011c8d311b2d24bec7f00d9d32918
Gift <aghaulor.gift@gmail.com>
2024-09-17 18:52:49 +0000

    stdin and stdout

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,10 @@
+// function that takes input and print output
+console.log('Welcome to Holberton School, what is your name?');
+
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  console.log(`Your name is: ${name}`);
+
+  console.log('This important software is now closing');
+  process.exit();
+});
