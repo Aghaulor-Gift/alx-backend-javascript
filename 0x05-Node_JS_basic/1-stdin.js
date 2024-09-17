ab640db0d83e6b3369019956d386e86fe42a4b4a
Gift <aghaulor.gift@gmail.com>
2024-09-17 19:58:18 +0000

    Update function

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,25 @@
 // function that takes input and print output
-console.log('Welcome to Holberton School, what is your name?');
+const readline = require('readline');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  console.log(`Your name is: ${name}`);
+function handleInput() {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
 
-  console.log('This important software is now closing');
-  process.exit();
-});
+  rl.question('Welcome to Holberton School, what is your name?\n', (name) => {
+    console.log(`Your name is: ${name}`);
+    rl.close();
+  });
+
+  rl.on('close', () => {
+    console.log('This important software is now closing');
+    process.exit(0);
+  });
+}
+
+module.exports = handleInput;
+
+if (require.main === module) {
+  handleInput();
+}
