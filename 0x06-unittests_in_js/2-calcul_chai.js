f8a61948e5f7ea50eb21dc2bfe6ca2698bba881e
Gift <aghaulor.gift@gmail.com>
2024-09-21 13:02:25 +0000

    Update

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,23 +1,17 @@
 // Upgrade the function you created in the previous task (0-calcul.js)
 // Add a new argument named type at first argument of the function.
 // type can be SUM, SUBTRACT, or DIVIDE (string)
-function calculateNumber(type, a, b) {
-  const roundedA = Math.round(a);
-  const roundedB = Math.round(b);
-
-  switch (type) {
-    case 'SUM':
-      return roundedA + roundedB; // Standard summation
-    case 'SUBTRACT':
-      return roundedA - roundedB; // Standard subtraction
-    case 'DIVIDE':
-      if (roundedB === 0) {
-        return 'Error'; // Handle division by zero
-      }
-      return roundedA / roundedB; // Standard division
-    default:
-      throw new Error('Invalid operation type');
+const calculateNumber = (type, a, b) => {
+  if (type === 'SUM') {
+    return Math.round(a) + Math.round(b);
   }
-}
+  if (type === 'SUBTRACT') {
+    return Math.round(a) - Math.round(b);
+  }
+  if (type === 'DIVIDE') {
+    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
+  }
+  return 0;
+};
 
 module.exports = calculateNumber;
