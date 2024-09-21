06c624a38f5afd4527ce802274c31c213f65fbc1
Gift <aghaulor.gift@gmail.com>
2024-09-21 14:49:23 +0000

    api.test.js

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,19 +1,14 @@
-const chai = require('chai');
-const chaiHttp = require('chai-http');
-const app = require('./api'); // Import the express app
+const request = require('request');
+const { expect } = require('chai');
 
-// Use chaiHttp for making HTTP requests in Chai tests
-chai.use(chaiHttp);
-const { expect } = chai;
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-describe('Index page', () => {
-  it('should return the correct status code and message', (done) => {
-    chai.request(app)
-      .get('/')
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.text).to.equal('Welcome to the payment system');
-        done();
-      });
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
   });
 });
